1c89c36d887ac5bb33950a35b59ccb60bbecdbdf
Your Name <you@example.com>
2022-02-08 09:56:48 +0100

    solo aws

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,28 @@
-#Información de la parte de red
+#Información de la parte de red de azure
+# output "vnet_name" {
+#   description = "Nombre de la Vnet creada para el Oracle"
+#   value       = module.red.vnetname
+# }
+# output "vnet_CIDR" {
+#   description = "CIDR de la vnet "
+#   value       = module.red.vnetaddress
+# }
 
-output "vnet_name" {
-  description = "Nombre de la Vnet creada para el Oracle"
-  value       = module.red.vnetname
-}
-output "vnet_CIDR" {
-  description = "CIDR de la vnet "
-  value       = module.red.vnetaddress
-}
+# output "Subnet_Name" {
+#   description = "Nombre de la subred creada "
+#   value       = module.red.subnetname
+# }
 
-output "Subnet_Name" {
-  description = "Nombre de la subred creada "
-  value       = module.red.subnetname
-}
-
-output "Subnet_CIDR" {
-  description = "CIDR de la subnet "
-  value       = module.red.subnetprefix
-}
-output "vnet_dedicated_security_group_Name" {
-  description = "Nombre del grupo de seguridad aplicado a la subred "
-  value       = module.red.sgname
-}
-output "vnet_dedicated_security_ingress_rules" {
-  description = "Reglas de entrada del grupo de seguridad "
-  value       = module.red.sgirules
-}
+# output "Subnet_CIDR" {
+#   description = "CIDR de la subnet "
+#   value       = module.red.subnetprefix
+# }
+# output "vnet_dedicated_security_group_Name" {
+#   description = "Nombre del grupo de seguridad aplicado a la subred "
+#   value       = module.red.sgname
+# }
+# output "vnet_dedicated_security_ingress_rules" {
+#   description = "Reglas de entrada del grupo de seguridad "
+#   value       = module.red.sgirules
+# }
 
